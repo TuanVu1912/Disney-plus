7f6ce09e8f3b19dd6d3e1ffd98732f1875e008b9
Tuanvu1912 <tuanvu98.work@gmail.com>
2021-05-06 11:12:21 +0700

    Add : Movie

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Detail from "./components/Detail";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Detail from "./components/Detail";
 import Login from "./components/Login";
 
 function App() {
