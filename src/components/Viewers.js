f8fec9cbaff2eece23957148fdb7acb6eea46733
Tuanvu1912 <tuanvu98.work@gmail.com>
2021-05-06 20:07:02 +0700

    Edit: background video

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -45,9 +45,10 @@ export default Viewers;
 
 const Container = styled.div`
   margin-top: 30px;
+  padding: 30px 0px 26px;
   display: grid;
-  padding: 30px 0 26px;
   grid-gap: 25px;
+  gap: 25px;
   grid-template-columns: repeat(5, minmax(0, 1fr));
   @media (max-width: 768px) {
     grid-template-columns: repeat(1, minmax(0, 1fr));
@@ -57,37 +58,41 @@ const Container = styled.div`
 const Wrap = styled.div`
   padding-top: 56.25%;
   border-radius: 10px;
-  cursor: pointer;
-  border: 3px solid rgba(249, 249, 249, 0.1);
   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  cursor: pointer;
   overflow: hidden;
   position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
   img {
-    inset: 0;
+    inset: 0px;
     display: block;
-    width: 100%;
     height: 100%;
     object-fit: cover;
     opacity: 1;
     position: absolute;
-    transition: opacity 500ms ease-ease-in-out 0s;
+    transition: opacity 500ms ease-in-out 0s;
+    width: 100%;
     z-index: 1;
     top: 0;
   }
   video {
     width: 100%;
     height: 100%;
-    object-fit: cover;
     position: absolute;
-    top: 0;
-    left: 0;
+    object-fit: cover;
+    top: 0px;
+    opacity: 0;
+    z-index: 0;
   }
   &:hover {
-    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
-      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
     transform: scale(1.05);
     border-color: rgba(249, 249, 249, 0.8);
+    video {
+      opacity: 1;
+    }
   }
 `;
